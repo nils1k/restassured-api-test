3825ffebaea3a380175dcf73742c759ac8ad8ba0
nils1k <github@kristiannilsen.no>
2017-05-25 13:42:56 +0200

    Cleanup in pom.xml, and add script for reporting integration test results to TestRail from CircleCI.

---
+++ src/main/script/ReportToTestRail.groovy
@@ -0,0 +1,71 @@
+import groovyx.net.http.RESTClient
+import org.apache.http.client.HttpResponseException
+
+import static groovyx.net.http.ContentType.*
+
+def buildNumber = System.getProperty("CIRCLE_BUILD_NUM")
+def circleCIResultFilesPath = "${System.getProperty("CIRCLE_TEST_REPORTS")}/junit/"
+
+// Only used for debugging at localhost
+def localResultFilesPath = "${System.getProperty("user.dir")}/target/failsafe-reports/"
+
+def junitResultFiles = new FileNameFinder().getFileNames(circleCIResultFilesPath, "TEST-*.xml")
+
+def testrailAPI = new RESTClient("https://knilsen.testrail.net/")
+def baseURI = "${testrailAPI.uri.toString()}index.php?/api/v2/"
+def authCredentials = "testrail@kristiannilsen.com:${System.getProperty("TestRailAPI")}".bytes.encodeBase64()
+
+testrailAPI.defaultRequestHeaders."Content-Type" = "application/json"
+testrailAPI.defaultRequestHeaders."Authorization" = "Basic ${authCredentials}"
+
+try {
+    def testSection = testrailAPI.post(
+            uri: new URI("${baseURI}add_section/1"),
+            body: [name: "CircleCI Integration Tests for Build Number ${buildNumber}"],
+            requestContentType: JSON
+    )
+
+    def testRun = testrailAPI.post(
+            uri: new URI("${baseURI}add_run/1"),
+            body: [name: "CircleCI Test Run for Build Number ${buildNumber}"],
+            requestContentType: JSON
+    )
+
+    junitResultFiles.each { resultFile ->
+        def result = new XmlSlurper().parse(resultFile)
+        def testsuiteName = result.@name
+
+        result.testcase.each { testcase ->
+            def status = 1
+            def description = "Test OK"
+            def testCaseName = testcase.@name
+
+            // Breadth-First Traversal to find any direct children
+            // '*' is shorthand for Breadth-First Traversal
+            def errorMessage =  testcase.'*'.find { testcase.error || testcase.failure }
+
+            if (!errorMessage.isEmpty()) {
+                status = 5
+                description = errorMessage
+            }
+
+            def testCase = testrailAPI.post(
+                    uri: new URI("${baseURI}add_case/${testSection.data.id}"),
+                    body: [title: testCaseName.toString()],
+                    requestContentType: JSON
+            )
+
+            testrailAPI.post(
+                    uri: new URI("${baseURI}add_result_for_case/${testRun.data.id}/${testCase.data.id}"),
+                    body: [status_id: status.toString(), comment: description.toString()],
+                    requestContentType: JSON
+            )
+        }
+    }
+} catch (HttpResponseException ex) {
+    r = ex.response
+    println("Success: $r.success")
+    println("Status:  $r.status")
+    println("Reason:  $r.statusLine.reasonPhrase")
+    println("Content: \n${JsonOutput.prettyPrint(JsonOutput.toJson(r.data))}")
+}
